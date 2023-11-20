9ec323de125e6798abac10c61f581859f2891dd8
Eddie Ward <eddie.musashi1@gmail.com>
2023-11-20 08:21:52 -0500

    Create index.py

---
+++ index.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+from glob import glob
+
+import IPython.display as ipd
+from tqdm import tqdm
+
+import subprocess
+
+plt.style.use('ggplot')
+
