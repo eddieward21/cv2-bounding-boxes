3abc2b89466177001f93291138a42a6a24597c94
Eddie Ward <eddie.musashi1@gmail.com>
2023-11-20 08:36:02 -0500

    Update index.py

--- index.py
+++ index.py
@@ -79,3 +79,117 @@ video_labels = (
     labels.query('videoName == "026c7465-309f6d33"').reset_index(drop=True).copy()
 )
 video_labels["video_frame"] = (video_labels["frameIndex"] * 11.9).round().astype("int")
+
+
+video_labels["category"].value_counts()
+
+
+cap = cv2.VideoCapture("026c7465-309f6d33.mp4")
+n_frames = int(cap.get(cv2.cv2.CAP_PROP_FRAME_COUNT))
+
+img_idx = 0
+for frame in range(n_frames):
+    ret, img = cap.read()
+    if ret == False:
+        break
+    if frame == 1035:
+        break
+cap.release()
+
+img_example = img.copy()
+frame_labels = video_labels.query('video_frame == 1035')
+for i, d in frame_labels.iterrows():
+    pt1 = int(d['box2d.x1']), int(d['box2d.y1'])
+    pt2 = int(d['box2d.x2']), int(d['box2d.y2'])
+    cv2.rectangle(img_example, pt1, pt2, (0, 0, 255), 3)
+
+display_cv2_img(img_example)
+
+color_map = {
+    "car": (0, 0, 255),
+    "truck": (0, 0, 100),
+    "pedestrian": (255, 0, 0),
+    "other vehicle": (0, 0, 150),
+    "rider": (200, 0, 0),
+    "bicycle": (0, 255, 0),
+    "other person": (200, 0, 0),
+    "trailer": (0, 150, 150),
+    "motorcycle": (0, 150, 0),
+    "bus": (0, 0, 100),
+}
+
+img_example = img.copy()
+frame_labels = video_labels.query('video_frame == 1035')
+for i, d in frame_labels.iterrows():
+    pt1 = int(d['box2d.x1']), int(d['box2d.y1'])
+    pt2 = int(d['box2d.x2']), int(d['box2d.y2'])
+    color = color_map[d['category']]
+    cv2.rectangle(img_example, pt1, pt2, color, 3)
+
+display_cv2_img(img_example)
+
+frame_labels = video_labels.query("video_frame == @frame")
+font = cv2.FONT_HERSHEY_TRIPLEX
+img_example = img.copy()
+for i, d in frame_labels.iterrows():
+    pt1 = int(d["box2d.x1"]), int(d["box2d.y1"])
+    pt2 = int(d["box2d.x2"]), int(d["box2d.y2"])
+    color = color_map[d["category"]]
+    img_example = cv2.rectangle(img_example, pt1, pt2, color, 3)
+    pt_text = int(d["box2d.x1"]) + 5, int(d["box2d.y1"] + 10)
+    img_example = cv2.putText(img_example, d["category"], pt_text, font, 0.5, color)
+display_cv2_img(img_example)
+cap.release()
+
+def add_annotations(img, frame, video_labels):
+    max_frame = video_labels.query("video_frame <= @frame")["video_frame"].max()
+    frame_labels = video_labels.query("video_frame == @max_frame")
+    for i, d in frame_labels.iterrows():
+        pt1 = int(d["box2d.x1"]), int(d["box2d.y1"])
+        pt2 = int(d["box2d.x2"]), int(d["box2d.y2"])
+        color = color_map[d["category"]]
+        img = cv2.rectangle(img, pt1, pt2, color, 3)
+    return img
+
+VIDEO_CODEC = "mp4v"
+fps = 59.94
+width = 1280
+height = 720
+out = cv2.VideoWriter("out_test.mp4",
+                cv2.VideoWriter_fourcc(*VIDEO_CODEC),
+                fps,
+                (width, height))
+
+cap = cv2.VideoCapture("026c7465-309f6d33.mp4")
+n_frames = int(cap.get(cv2.cv2.CAP_PROP_FRAME_COUNT))
+
+for frame in tqdm(range(n_frames), total=n_frames):
+    ret, img = cap.read()
+    if ret == False:
+        break
+    img = add_annotations(img, frame, video_labels)
+    out.write(img)
+out.release()
+cap.release()
+
+tmp_output_path = "out_test.mp4"
+output_path = "out_test_compressed.mp4"
+subprocess.run(
+    [
+        "ffmpeg",
+        "-i",
+        tmp_output_path,
+        "-crf",
+        "18",
+        "-preset",
+        "veryfast",
+        "-vcodec",
+        "libx264",
+        output_path,
+        '-loglevel',
+        'quiet'
+    ]
+)
+
+ipd.Video('out_test_compressed.mp4', width=600)
+
