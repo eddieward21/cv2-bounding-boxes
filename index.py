ccdddd5febf81318ac9dc371786ea1116b748ef2
Eddie Ward <eddie.musashi1@gmail.com>
2023-11-20 08:31:13 -0500

    Update index.py

--- index.py
+++ index.py
@@ -12,3 +12,13 @@ import subprocess
 
 plt.style.use('ggplot')
 
+input_file = '../input/driving-video-with-object-tracking/bdd100k_videos_train_00/bdd100k/videos/train/026c7465-309f6d33.mov'
+subprocess.run(['ffmpeg',
+                '-i',
+                input_file,
+                '-qscale',
+                '0',
+                '026c7465-309f6d33.mp4',
+                '-loglevel',
+                'quiet']
+              )
